54dd41600059234af76bdfb9c102c3992b537c7f
maxflex <makcyxa-k@yandex.ru>
2022-07-28 16:16:39 +0300

    Add SPB dest

--- app/Enums/Destination.php
+++ app/Enums/Destination.php
@@ -4,10 +4,13 @@ namespace App\Enums;
 
 use BenSampo\Enum\Enum;
 
+// https://ticket.rzd.ru/api/v1/suggests?Query=Москва
+// или на https://ticket.rzd.ru/ при вводе пункта назначения в suggest-запросе
 final class Destination extends Enum
 {
-    const bel = '2014370';
     const mos = '2000000';
+    const spb = '2004000';
+    const bel = '2014370';
     const kur = '2000150';
     const minsk = '2100001';
 }
