e36977adf8e8a6468407d5d01e522ea82b4c363f
maxflex <makcyxa-k@yandex.ru>
2022-08-19 10:01:28 +0300

    Rename mos

--- app/Enums/Destination.php
+++ app/Enums/Destination.php
@@ -8,7 +8,7 @@ use BenSampo\Enum\Enum;
 // или на https://ticket.rzd.ru/ при вводе пункта назначения в suggest-запросе
 final class Destination extends Enum
 {
-    const mos = '2000000';
+    const msk = '2000000';
     const spb = '2004000';
     const bel = '2014370';
     const kur = '2000150';
